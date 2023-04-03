c380d18a144a992c506ba0dc5e54dd8491a5bdd4
agmardones97 <84813173+agmardones97@users.noreply.github.com>
2023-04-03 11:04:31 -0400

    Add files via upload

---
+++ 06-mis-custom-hooks/useTodo.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer} from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+  };
+
+export const useTodo = (initialState = []) => {
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  const todosCount = todos.length
+
+  const pendingTodosCount = todos.filter(todo => !todo.done).length
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  return {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo
+  };
+};
